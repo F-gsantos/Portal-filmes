15e499a940437d832632012154af3a0eaf7c6b53
Fabrício Gomes <fabricio.gsantos16@gmail.com>
2024-11-04 13:05:55 -0300

    ajuste

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,7 @@ export default function Login({ isLogged, handleLogin }) {
             {isLogged && <p>Olá, usuário</p>}
             <button
                 onClick={handleLogin}
-                className={`${isLogged ? "bg-white" : " border-b-zinc-950"} bg-red-700 px-4 py-1 rounded`}>
+                className={`${isLogged ? "bg-red-700" : " border-b-zinc-950"} bg-red-700 px-4 py-1 rounded`}>
                 {isLogged ? "Logout" : "Login"}
             </button>
         </div>
